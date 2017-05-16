f89da2ec68a9548227b56a4597a33d6b4def5585
Patrick <pmoore4181@gmail.com>
2017-05-15 23:04:19 -0500

    finished html, js. could use some more styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+
+var goalNumber;
+var crystalOne;
+var crystalTwo;
+var crystalThree;
+var crystalFour;
+var userScore = 0;
+var wins = 0;
+	$("#yourWins").html(wins);
+var losses = 0;
+	$("#yourLosses").html(losses);
+
+// create random numbers and assign them to each crystal
+function createCrystalValue() {
+	crystalOne = Math.floor(Math.random() * 12) + 1;
+	crystalTwo = Math.floor(Math.random() * 12) + 1;
+	crystalThree = Math.floor(Math.random() * 12) + 1;
+	crystalFour = Math.floor(Math.random() * 12) + 1;
+};
+createCrystalValue();
+
+// create random number for each round
+function createGoal() {
+	goalNumber = Math.floor(Math.random() * 101) + 19;
+	$("#goal").html(goalNumber);
+};
+createGoal();
+
+// when clicking each crystal, use the value, increase userScore and show in html
+function assignCrystalValue() {
+
+	$("#firstCrystal").on("click", function () {
+		console.log(crystalOne);
+		userScore = userScore + crystalOne;
+		$("#yourScore").html(userScore);
+	});
+
+	$("#secondCrystal").on("click", function () {
+		console.log(crystalTwo);
+		userScore = userScore + crystalTwo;
+		$("#yourScore").html(userScore);
+	});
+
+	$("#thirdCrystal").on("click", function () {
+		console.log(crystalThree);
+		userScore = userScore + crystalThree;
+		$("#yourScore").html(userScore);
+	});
+
+	$("#fourthCrystal").on("click", function () {
+		console.log(crystalFour);
+		userScore = userScore + crystalFour;
+		$("#yourScore").html(userScore);
+	});
+
+	$("#yourScore").html(userScore);
+};
+assignCrystalValue();
+
+// compare scores 
+function compareScores() {
+	$(".crystal").on("click", function () {
+		// if userScore == goalNumber, increase wins, create new goalNumber and crystal value. reset userScore
+		if (userScore == goalNumber) {
+			wins = wins + 1;
+			$("#yourWins").html(wins);
+			userScore = 0;
+			$("#yourScore").html(userScore);
+			createGoal();
+			$("#goal").html(goalNumber);
+			createCrystalValue();
+			alert("You Won! Wahoo!");
+		}
+
+		// if userScore > goalNumber, losses increase, create new goalNumber and crystal value. reset userScore
+		if (userScore > goalNumber) {
+			losses = losses + 1;
+			$("#yourLosses").html(losses);
+			userScore = 0;
+			$("#yourScore").html(userScore);
+			createGoal();
+			$("#goal").html(goalNumber);
+			createCrystalValue();
+			alert("Oh no! You lost!")
+		}
+	});
+
+};
+compareScores();	
+
+
+
+});
